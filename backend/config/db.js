7fb0ef7df7db6b3b0e63067e33fda8fafbb6398f
costel-dev <costelcuconoiu@gmail.com>
2020-12-28 16:45:46 +0100

    setup our MongoDB database and connected to our server

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import { DH_UNABLE_TO_CHECK_GENERATOR } from "constants";
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    } catch (error) {
+        console.error(`Error: ${error.message}`.red.underline.bold);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
